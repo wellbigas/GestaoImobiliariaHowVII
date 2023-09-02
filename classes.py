d957375d10f2692a0a0d67e14f4590c3ed76f580
Wellington Luís Moreno Bigas <wellington.bigas@gmail.com>
2023-09-02 18:12:52 -0300

    Adicionado classe para consulta analógia no banco de dados utilizando JOIN

--- classes.py
+++ classes.py
@@ -8,4 +8,18 @@ class Pagamento:
     def __str__(self):
         return f"ID: {self.id_pagamento}, Data: {self.data_pagamento}, Valor: {self.valor_pagamento}, Código do Imóvel: {self.codigo_imovel}"
 
+class EstruturaAnalogica:
+    def __init__(self, id_pagamento, data_pagamento, valor_pagamento, id_imovel, descricao_imovel, nome_tipo_imovel):
+        self.id_pagamento = id_pagamento
+        self.data_pagamento = data_pagamento
+        self.valor_pagamento = valor_pagamento
+        self.id_imovel = id_imovel
+        self.descricao_imovel = descricao_imovel
+        self.nome_tipo_imovel = nome_tipo_imovel
+
+    def __str__(self):
+        return f"ID do Pagamento: {self.id_pagamento}, Data de Pagamento: {self.data_pagamento}, " \
+               f"Valor do Pagamento: {self.valor_pagamento}, ID do Imóvel: {self.id_imovel}, " \
+               f"Descrição do Imóvel: {self.descricao_imovel}, Tipo de Imóvel: {self.nome_tipo_imovel}"
+
 
